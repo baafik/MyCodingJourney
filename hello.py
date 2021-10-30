9d4242c548bd851292aab88d387a1005ee000df7
baafik <baafik05@gmail.com>
2021-10-30 17:34:22 +0000

    I have studied python to into of variables at the time of this commit

--- hello.py
+++ hello.py
@@ -8,4 +8,38 @@ if 5>2:
 
  #Variables
  x = 5
- y = "Hello, World!" 
+ y = "Hello, World!"
+
+ """
+ This is 
+ a multiline 
+ comment
+ """ 
+ 
+#Creating variables of all kinds
+x = 5
+y = "clement" 
+print(x)
+print(y)
+
+x = 4 # x is an int 
+x = "sally" # x is now a str
+print(x)
+
+#casting
+x = str(3) # x will 3
+y = int(3) # y will be 3
+z = float(3) # y will be 3.0
+print(z)
+
+# type of variable
+print(type(x))
+print(type(y))
+
+#single & double qoutes
+x = "clement"
+y = 'clement'
+
+A = 4
+a = sally 
+# A will not overide a 
