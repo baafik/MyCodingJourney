3edc247f88bff479daf593b3d4888324fe6349f0
Parabeac-Nest <admin@parabeac.com>
2022-11-15 07:22:33 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/hotel_text_styles.g.dart
@@ -0,0 +1,20 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class HotelTextStyles {
+
+static const TextStyle defaultRegularBody = TextStyle(
+  fontSize: 17,
+  fontWeight: FontWeight.w400,
+  letterSpacing: -0.40799999237060547,
+  fontFamily: 'SF Pro Text',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
